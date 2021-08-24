cebd648c2c5e023a37a8126b31f3004ea4e9ca1c
katopz <katopz@gmail.com>
2021-08-24 15:35:22 +0700

    Add get event from api

---
+++ src/alpaca/utils/events.ts
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+import { Chain } from "@defillama/sdk/build/general";
+import { ethers } from "ethers";
+import { Interface } from "ethers/lib/utils";
+
+const GETBLOCK_API_KEY = process.env.GETBLOCK_API_KEY
+const RPC_URL = `https://bsc.getblock.io/mainnet/?api_key=${GETBLOCK_API_KEY}`
+
+// 0x98172b
+export const getPositionIds = async (address: string, blockNumber: number, block = 'latest', chain: Chain = 'bsc'): Promise<any> => {
+  const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+  const result = await provider.getLogs({
+    address,
+    fromBlock: blockNumber,
+    toBlock: blockNumber,
+    topics: [
+      '0x73c4ef442856bea52a6b34a83f35484ee65828010254ec27766c5a8c13db6c84',
+    ]
+  });
+
+  const iface = new Interface([{
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "loan",
+        "type": "uint256"
+      }
+    ],
+    "name": "Work",
+    "type": "event"
+  }]);
+
+  const decodedEventLog = iface.decodeEventLog("Work", result[0].data, result[0].topics);
+  console.log('position id:', parseInt(decodedEventLog.id))
+
+  return [parseInt(decodedEventLog.id)]
+}
