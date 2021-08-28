0c4f5d157f7c17f0d6dd9dc6f6d4a394e42790b7
katopz <katopz@gmail.com>
2021-08-28 15:07:24 +0700

    refactor output

--- src/alpaca/users/summary.ts
+++ src/alpaca/users/summary.ts
@@ -6,48 +6,75 @@
 
       deposits: [
         {
+          positionId: "9967403",
+
           vaultAddress: "0x3fc149995021f1d7aec54d015dad3c7abc952bf0",
+          stratAddress: "0x50380Ac8DA73D73719785F0A4433192F4e0E6c90",
+
           tokenSymbol: "ALPACA",
           tokenAddress: "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F",
           tokenAmount: 695.245603609934955053,
-          priceUSD: 1042.8684054149,
-          block_timestamp: "2021-08-07T14:45:51.000Z",
-          block_number: "10277278",
-          block_hash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
+
+          tokenPriceUSD: 1042.8684054149,
+          tokenCurrentPriceUSD: 500,
+          tokenRecordedPriceUSD: 400,
+
+          blockNumber: "10277278",
+          blockHash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
+          blockTimestamp: "2021-08-07T14:45:51.000Z",
         },
         {
+          positionId: "9967403",
           vaultAddress: "0x7c9e73d4c71dae564d41f78d56439bb4ba87592f",
+          stratAddress: "0x50380Ac8DA73D73719785F0A4433192F4e0E6c90",
           tokenSymbol: "BUSD",
           tokenAddress: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
           tokenAmount: 504.503765091159716219,
-          priceUSD: 504.503765091159716219,
+          tokenPriceUSD: 504.503765091159716219,
+          block_number: "10277278",
           block_timestamp: "2021-08-07T14:45:51.000Z",
           block_hash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
         },
         {
+          positionId: "1967402",
           vaultAddress: "0x3fc149995021f1d7aec54d015dad3c7abc952bf0",
+          stratAddress: "0x50380Ac8DA73D73719785F0A4433192F4e0E6c90",
           tokenSymbol: "ALPACA",
           tokenAddress: "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F",
           tokenAmount: 577.406596699668932514,
           priceUSD: 519.6659370297,
+          block_number: "10277278",
           block_timestamp: "2021-08-11T06:37:01.000Z",
           block_hash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
         },
       ],
-      depositSummary: [
-        {
-          tokenSymbol: "ALPACA",
-          tokenAddress: "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F",
-          tokenAmount: 1,272.6522003096,
-          totalDepositUSD: 1527.12000,
-          block_hash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
-        },
-        {
-          tokenSymbol: "BUSD",
-          tokenAddress: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
-          tokenAmount: 504.503765091159716219,
-          totalDepositUSD: 504.503765091159716219,
-          block_hash: "0x9673166f4eb5e5f7a224d40ec2d3572777f51badf2e6ce7ed5bfb373b6325e06"
+      depositSummary: {
+        "9967403": {
+
+          depositAmount: 100,
+          equityAmount: 100,
+          debtAmount: 10,
+          profitAmount: 90,
+
+          depositUSD: 1000,
+          equityUSD: 1000,
+          debtUSD: 100,
+          profitUSD: 900,
+
+          tokens: [
+            {
+              tokenSymbol: "ALPACA",
+              tokenAddress: "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F",
+              tokenAmount: 1272.6522003096,
+              tokenPriceUSD: 1527.12000,
+            },
+            {
+              tokenSymbol: "BUSD",
+              tokenAddress: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+              tokenAmount: 504.503765091159716219,
+              tokenPriceUSD: 504.503765091159716219,
+            }
+          ]
         }
       ]
 
