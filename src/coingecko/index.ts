8b7fe583a3c2f0ccaa2844cee725c57488606d07
katopz <katopz@gmail.com>
2021-09-10 17:18:39 +0700

    Use json

--- src/coingecko/index.ts
+++ src/coingecko/index.ts
@@ -1,16 +1,5 @@
 import fetch from 'node-fetch'
-
-const PRICE_SYMBOL_MAP = {
-  BNB: 'wbnb',
-  wBNB: 'wbnb',
-  CAKE: 'pancakeswap-token',
-  ALPACA: 'alpaca-finance',
-  BUSD: 'binance-usd',
-  USDT: 'tether',
-  TUSD: 'true-usd',
-  BTCB: 'bitcoin-bep2',
-  ETH: 'ethereum',
-}
+import PRICE_SYMBOL_MAP from '../coingecko/tokens.id.json'
 
 export const fetchPriceUSD = async (ids?: string[]) => {
   ids = ids.map(id => PRICE_SYMBOL_MAP[id]) || Object.values(PRICE_SYMBOL_MAP)
