a18a605e450976edc3aaa04f6fd9a7f023cf5e8d
katopz <katopz@gmail.com>
2021-09-12 12:54:58 +0700

    no use profitValueUSD

--- src/alpaca/users/investment.ts
+++ src/alpaca/users/investment.ts
@@ -21,7 +21,6 @@ export interface IFarmInvestmentInfo extends IUserInvestmentInfo {
 
   depositValueUSD: number // 1000,
   equityValueUSD: number // 1000,
-  profitValueUSD: number // 900,
 
   stratAddress: string // "0x50380Ac8DA73D73719785F0A4433192F4e0E6c90",
   stratSymbol: string // "CAKE",
@@ -126,7 +125,6 @@ export const getUserInvestmentInfos = async (transactionTransferInfo: ITransacti
 
           depositValueUSD: _.sumBy(e.transferInfos, 'tokenValueUSD') ?? 0,
           equityValueUSD: _.sumBy(e.transferInfos, 'equityValueUSD') ?? 0,
-          profitValueUSD: _.sumBy(e.transferInfos, 'profitValueUSD') ?? 0,
         } as IFarmInvestmentInfo
       case InvestmentTypeObject.lend:
         const lendTx = e as unknown as ILendTransaction
