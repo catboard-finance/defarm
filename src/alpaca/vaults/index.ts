b9cbf8d4aefe73ec9a2c05b6760c0ef9ecf41eeb
katopz <katopz@gmail.com>
2021-09-23 11:33:36 +0700

    won't filter 0 tx value

--- src/alpaca/vaults/index.ts
+++ src/alpaca/vaults/index.ts
@@ -1,6 +1,5 @@
 require('dotenv').config()
 import alpacaInfo from '../info.mainnet.json'
-import { stringToFloat } from '../utils/converter';
 import { ITransfer } from '../../type';
 import _ from 'lodash'
 import { FAIR_LAUNCH_ADDRESS } from '../core';
@@ -21,8 +20,4 @@ export const filterDepositVaults = (txList: ITransfer[]) => txList.filter(tx =>
   ALPACA_VAULT_ADDRESSES.includes(tx.to_address.toLowerCase())
 )
 
-export const filterNoZeroTransfer = (txList: ITransfer[]) => txList.filter(tx =>
-  stringToFloat(tx.value) > 0
-)
-
-export const filterInvestmentTransfers = (transfers: ITransfer[]) => filterNoZeroTransfer(filterVaults(transfers))
+export const filterInvestmentTransfers = (transfers: ITransfer[]) => filterVaults(transfers)
